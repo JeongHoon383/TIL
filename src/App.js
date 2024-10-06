4650faa963fd584c0217dac20d7f296b23811538
Jeong Hoon <gbg9609@naver.com>
2024-10-06 19:36:07 +0900

    리액트를 다루는 기술 5장 ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import { Component } from "react";
-import ValidationSample from "./component/5장. ref: DOM에 이름 달기/ValidationSample";
+import ScrollBox from "./component/5장. ref: DOM에 이름 달기/ScrollBox";
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
   }
 }
 
