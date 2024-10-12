16a0f50983f6626dd45b281f9704e8617b518770
Jeong Hoon <gbg9609@naver.com>
2024-10-13 07:48:28 +0900

    리액트를 다루는 기술 - 7장. 컴포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Component } from "react";
-import IterationSample from "./component/6장. 컴포넌트 반복/IterationSample";
+import LifeCycleSample from "./component/7장. 컴포넌트의 라이프사이클 메서드/LifeCycleSample";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <IterationSample />
+        <LifeCycleSample />
       </div>
     );
   }
--- src/component/6장. 컴포넌트 반복/IterationSample.js
+++ src/component/6장. 컴포넌트 반복/IterationSample.js
@@ -1,8 +1,6 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 
 const IterationSample = () => {
-  const inputRef = useRef();
-
   const [names, setNames] = useState([
     { id: 1, text: "눈사람" },
     { id: 2, text: "얼음" },
@@ -12,9 +10,35 @@ const IterationSample = () => {
   const [inputText, setInputText] = useState("");
   const [nextId, setNextId] = useState(5);
 
-  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+  const onChange = (e) => setInputText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId, // nextId 값을 id로 설정
+      text: inputText,
+    });
+    setNextId(nextId + 1); // nextId 값에 1을 더해 준다.
+    setNames(nextNames); // names 값을 업데이트한다.
+    setInputText(""); // inputText를 비운다.
+  };
 
-  return <ul>{nameList}</ul>;
+  const onRemove = (id) => {
+    const nextNames = names.filter((name) => name.id !== id);
+    setNames(nextNames);
+  };
+
+  const nameList = names.map((name) => (
+    <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+      {name.text}
+    </li>
+  ));
+
+  return (
+    <>
+      <input value={inputText} onChange={onChange} />
+      <button onClick={onClick}>추가</button>
+      <ul>{nameList}</ul>
+    </>
+  );
 };
 
 export default IterationSample;
