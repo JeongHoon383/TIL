6fc8389d2ade60a1c9f40d7bf435197cf57a2366
Jeong Hoon <gbg9609@naver.com>
2024-10-16 08:17:30 +0900

    8장 hooks - useMemo, useCallback, useRef, 커스텀 Hooks 만들기

--- src/component/8장. Hooks/Average.js
+++ src/component/8장. Hooks/Average.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 const getAverage = (numbers) => {
   console.log("평균값 계산 중..");
@@ -10,19 +10,33 @@ const getAverage = (numbers) => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     setNumber(e.target.value);
-  };
-  const onInsert = (e) => {
-    const nextList = list.concat(parseInt(number));
-    setList(nextList);
-    setNumber("");
-  };
+  }, []); // 컴포넌트가 처음 렌더링될 때만 함수 생성
+
+  const onInsert = useCallback(
+    (e) => {
+      const nextList = list.concat(parseInt(number));
+      setList(nextList);
+      setNumber("");
+      inputEl.current.focus();
+    },
+    [number, list]
+  ); // number 혹은 list가 바뀌었을 때만 함수 생성
+
+  // 컴포넌트가 리렌더링 될 때마다 함수도 새로 만들어짐 - 리액트 특징
+  // 연산을 할 때만 함수가 생성되도록 useCallback을 통해 최적화
+
+  const avg = useMemo(() => getAverage(list), [list]);
+  // input 내용이 수정될 때도 getAverage 함수가 호출됨
+  // useMemo를 특정 값이 바뀌었을 때만 연산 실행
+  // list 배열의 내용이 바뀔 때만 getAverage 함수가 호출
 
   return (
     <div>
-      <input value={number} onChange={onChange} />
+      <input value={number} onChange={onChange} ref={inputEl} />
       <button onClick={onInsert}>등록</button>
       <ul>
         {list.map((value, index) => (
@@ -31,7 +45,7 @@ const Average = () => {
       </ul>
       <div>
         <b>평균값 : </b>
-        {getAverage(list)}
+        {avg}
       </div>
     </div>
   );
